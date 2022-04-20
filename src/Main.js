39650e36b1006339cab669b03a7c6f499a265b79
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-21 02:40:19 +0900

    feat: cardを使ってyoutubeのような構成を試す

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
-import { Button, Caption, Headline, Paragraph, Subheading, Title } from 'react-native-paper';
+import { Avatar, Button, Card, Paragraph, Subheading } from 'react-native-paper';
 
 export const Main = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Headline>Paper紹介</Headline>
-        <Title>タイトル</Title>
-        <Paragraph>これが段落です。</Paragraph>
-        <Paragraph>じゅげむじゅげむ　ごこうのすりきれ</Paragraph>
-        <Caption>キャプションもかけますと。</Caption>
-        <Subheading>Button</Subheading>
         <Subheading>Card</Subheading>
-        <Button>普通のボタン(mode: text)</Button>
-        <Button mode="contained">containedなボタン</Button>
-        <Button mode="outlined">outlinedなボタン</Button>
+        <Card>
+          <Card.Cover source={require('../assets/ramen.jpeg')} />
+            <Card.Title
+              title="ラーメン紀行 ２日目"
+              subtitle="炎のラーメンちゃんねる"
+              left={props => <Avatar.Text {...props} label="YN" />}
+            />
+            <Card.Content>
+              <Paragraph>
+                とうとうこの伝説のお店にやってきました。
+              </Paragraph>
+            </Card.Content>
+            <Card.Actions style={{ justifyContent: 'flex-end' }}>
+              <Button>みない</Button>
+              <Button mode="contained">見る</Button>
+            </Card.Actions>
+        </Card>
       </ScrollView>
     </SafeAreaView>
   );
