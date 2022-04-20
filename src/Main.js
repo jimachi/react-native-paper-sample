d70bd3b66825c9cf5e1c365f2f85224c98447bc1
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-21 02:28:41 +0900

    feat: 基本的な機能を試してみる

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
+import { Button, Caption, Headline, Paragraph, Subheading, Title } from 'react-native-paper';
 
 export const Main = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        {/* これから実装する */}
+        <Headline>Paper紹介</Headline>
+        <Title>タイトル</Title>
+        <Paragraph>これが段落です。</Paragraph>
+        <Paragraph>じゅげむじゅげむ　ごこうのすりきれ</Paragraph>
+        <Caption>キャプションもかけますと。</Caption>
+        <Subheading>Button</Subheading>
+        <Subheading>Card</Subheading>
+        <Button>普通のボタン(mode: text)</Button>
+        <Button mode="contained">containedなボタン</Button>
+        <Button mode="outlined">outlinedなボタン</Button>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
