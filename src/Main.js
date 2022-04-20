2eb3d837abc464699b92cbc42e58a49d1ca748a3
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-21 02:09:12 +0900

    first commit

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+
+export const Main = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        {/* これから実装する */}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
