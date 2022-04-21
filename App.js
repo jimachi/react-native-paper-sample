e4d7fa257dfbb1b60e2dbd37e57efe5e0ca5539e
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-22 02:36:39 +0900

    feat: stack navigatorのテスト

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Main } from './src/Main';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Home Screen</Text>
+      <Button
+        title="詳細へ"
+        onPress={() => navigation.navigate('Detail')}
+      />
+    </View>
+  );
+};
+
+function DetailScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Detail Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <PaperProvider>
-      <Main />
-    </PaperProvider>
+      <Stack.Navigator
+        initialRouteName="Home"
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'ホームだよん' }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{ title: '詳細だよ'}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
