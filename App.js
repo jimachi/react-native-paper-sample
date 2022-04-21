8e6568271da58ca169372254352ebd2bef590977
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-22 02:11:52 +0900

    feat: ルーティング系のライブラリを追加

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Main } from './src/Main';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-   <PaperProvider>
-     <Main />
-   </PaperProvider>
+    <NavigationContainer>
+    <PaperProvider>
+      <Main />
+    </PaperProvider>
+    </NavigationContainer>
   );
 }
