a310b950a38044d805328be9970a26b5dc3a8aa4
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-26 00:42:25 +0900

    feat: bottom-tabsのテスト

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -39,24 +40,23 @@ function DetailScreen() {
 };
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-      >
-        <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'ホームだよん' }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="Detail"
           component={DetailScreen}
-          options={{ title: '詳細だよ'}}
+          options={{ title: '詳細だよ' }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
